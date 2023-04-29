e4848990b7e81c62029954cbabadfb141c437271
jlaz <01178193@pw.edu.pl>
2023-04-29 23:45:48 +0200

    Adding Second Class User

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        User a = new User();
+        a.Print("Hello World!");
     }
 }
---
+++ User.java
@@ -0,0 +1,12 @@
+public class User {
+    private String name;
+    private int age;
+
+    public static void User(String[] args) {
+
+    }
+
+    public static void Print(String message) {
+        System.out.println(message);
+    }
+}
